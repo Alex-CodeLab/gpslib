8c6522bba02540b626e9bb5d9057a55d5e6f7930
alex <1678423+alex-codelab@users.noreply.github.com>
2023-04-21 13:36:09 +0200

    frontend template

--- frontend/main.py
+++ frontend/main.py
@@ -1,130 +1,15 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 
-app = FastAPI()
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+from fastapi.templating import Jinja2Templates
+
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
+
+app = FastAPI(title="Dashboard",)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Pylot</title>
-        	<meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            
-            <link href="/static/css/app.css" rel="stylesheet" />
-            <link href="/static/css/apexcharts.css" rel="stylesheet" />            
-            <link href="/static/css/custom.css" rel="stylesheet" />
-
-            <script src="/static/js/app.js" ></script>
-            <script src="/static/js/apexcharts.min.js" ></script>
-            
-    </head>
-    
-    
-    <body>    
-    	<div class="wrapper">
-		<nav id="sidebar" class="sidebar js-sidebar">
-			<div class="sidebar-content js-simplebar">
-			    <a class="sidebar-brand" href="index.html"> <span class="align-middle"> </span> </a>
-
-				<ul class="sidebar-nav">
-					<li class="sidebar-header">
-						Pages
-					</li>
-
-					<li class="sidebar-item active">
-						<a class="sidebar-link" href="index.html">
-                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
-                    </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-profile.html">
-                        <i class="align-middle" data-feather="user"></i> <span class="align-middle">Logs</span>
-                    </a>
-					</li>
-                </ul> 
-            </div>
-		</nav>
-		
-		
-		<div class="main">
-			<nav class="navbar navbar-expand navbar-light navbar-bg">
-				<a class="sidebar-toggle js-sidebar-toggle">
-          <i class="hamburger align-self-center"></i>
-        </a>
-				<div class="navbar-collapse collapse">
-					<ul class="navbar-nav navbar-align">
-						
-						<li class="nav-item dropdown">
-							<a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
-                <i class="align-middle" data-feather="settings"></i>
-              </a>
-
-							<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                                <span class="text-dark"> </span>
-                            </a>
-							<div class="dropdown-menu dropdown-menu-end">
-								<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
-								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
-								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="#">Log out</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		
-        		<div id="chart1">
-                </div>
-		
-		<main class="content">
-                
-            <div class="container-fluid p-0">
-                
-                
-                
-                <h2>: <span id="ws-id"></span></h2>
-                <form action="" onsubmit="sendMessage(event)">
-                    <input type="text" id="messageText" autocomplete="off"/>
-                    <button>Send</button>
-                </form>
-                <ul id='messages'>
-                </ul>
-                
-                <div class="wrapper">
-
-            
-        </main>
-        
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-        <script src="/static/js/custom.js" ></script>
-    </body>
-</html>
-"""
 
 
 class ConnectionManager:
@@ -149,9 +34,13 @@ class ConnectionManager:
 manager = ConnectionManager()
 
 
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
+@app.get("/", status_code=200)
+async def get(request: Request) -> dict:
+
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {"request": request, },
+    )
 
 
 @app.websocket("/ws/{client_id}")
--- frontend/readme.md
+++ frontend/readme.md
@@ -1,15 +1,17 @@
 
 
 
+uvicorn main:app --host 0.0.0.0 --port 80
+
 
 todo 
-[ ] index.html    startup screen
-[ ] log.html      error, gps
-[ ] websocket
-[ ] dashboard.html  meters, compass, etc. 
 
-[ ] fastAPI 
-[ ] servicectl 
+- [ ] index.html    startup screen
+- [ ] log.html      error, gps
+- [ ] websocket
+- [x] bootsrap5 css dashboard
+- [ ] meters, compass, etc. 
 
-
+- [x] fastAPI
+- [ ] servicectl 
 
