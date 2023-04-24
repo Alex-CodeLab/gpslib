81d63cc466c0baff1fbcb3041f342030a628d087
alex <1678423+alex-codelab@users.noreply.github.com>
2023-04-24 19:53:13 +0200

    zmq async receive

--- zeromq.py
+++ zeromq.py
@@ -1,5 +1,6 @@
 #!/home/debain/gpslib/env/bin/python
 import zmq
+import zmq.asyncio
 from config import IPADDRESS
 import asyncio
 import websockets
@@ -11,10 +12,8 @@ async def handler(websocket, path):
 
 class ZeroPubSub:
 
-    def __init__(self, publish_address, subscribe_address):
-        self.context = zmq.Context()
-        self.publish_socket = self.context.socket(zmq.XPUB)
-        self.publish_socket.bind(publish_address)
+    def __init__(self, subscribe_address):
+        self.context = zmq.asyncio.Context()
         self.subscribe_socket = self.context.socket(zmq.SUB)
         self.subscribe_socket.bind(subscribe_address)
         self.subscribe_socket.setsockopt(zmq.SUBSCRIBE, b"")
@@ -23,11 +22,12 @@ class ZeroPubSub:
     async def start(self):
         while True:
             message = await self.subscribe_socket.recv()
-            await self.publish_socket.send(message)
+            print(message)
+            await self.websocketserver.send(message)
 
 
 if __name__ == "__main__":
-    pubsub = ZeroPubSub(f"tcp://{IPADDRESS}:{PORT}", "inproc://messages")
+    pubsub = ZeroPubSub(f"tcp://{IPADDRESS}:{PORT}")
     asyncio.run(pubsub.start())
     asyncio.get_event_loop().run_until_complete(pubsub.websocketserver)
     asyncio.get_event_loop().run_forever()
